4bcea2da1495ab1af1acc41f10d083fd835e5b4f
manish <manish99150@gmail.com>
2020-10-07 23:09:02 +0530

    frontend marged for deployement

--- backend/src/main/java/com/manish/ecommerce/domain/Product.java
+++ backend/src/main/java/com/manish/ecommerce/domain/Product.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "products")
 public class Product {
-
+    @Id
     private String id;
     private String name;    
     private String category;
---
+++ backend/src/main/resources/static/precache-manifest.606aa4140c4369e3840d34d9280f419a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d404673a79665788226958f712bc2a89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb8cbcef5db15a833009",
+    "url": "/static/css/main.ce6d7954.chunk.css"
+  },
+  {
+    "revision": "e45d01893ea0c1e2674d",
+    "url": "/static/js/2.bcc53d51.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.bcc53d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8cbcef5db15a833009",
+    "url": "/static/js/main.4498d7c1.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
